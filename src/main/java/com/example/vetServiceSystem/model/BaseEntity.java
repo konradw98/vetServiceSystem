347e1cf664bfb5df33d5770233fa2113c57aaa6a
konrad <konradwodnicki98@gmail.com>
2021-05-10 09:28:38 +0100

    add base entity

---
+++ src/main/java/com/example/vetServiceSystem/model/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.example.vetServiceSystem.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+}
