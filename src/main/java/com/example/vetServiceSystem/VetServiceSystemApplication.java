4951988e9da835b7b8fba11b970e11123a8e929a
konrad <konradwodnicki98@gmail.com>
2021-05-10 09:10:45 +0100

    initial commit

---
+++ src/main/java/com/example/vetServiceSystem/VetServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.vetServiceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetServiceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vetServiceSystem/VetServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vetServiceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetServiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
