df212e47b34cfb0a0aba17577358dee7978edb68
konrad <konradwodnicki98@gmail.com>
2021-06-07 14:13:16 +0100

    fixing bug

--- src/main/java/com/example/vetServiceSystem/controllers/IndexController.java
+++ src/main/java/com/example/vetServiceSystem/controllers/IndexController.java
@@ -1,11 +1,14 @@
 package com.example.vetServiceSystem.controllers;
 
+import com.example.vetServiceSystem.repositories.OwnerRepository;
+import com.example.vetServiceSystem.repositories.PetRepository;
+import com.example.vetServiceSystem.repositories.VetRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+    
     @RequestMapping("index")
     public String index(){
 
